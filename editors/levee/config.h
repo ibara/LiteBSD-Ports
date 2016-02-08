2b7d7e6855011beb06eae84c812e8f4b63bbce4a
Brian Callahan <dodonpachi-github@mailinator.com>
2016-02-08 18:00:00 -0500

    Add levee, a BSD licensed vi clone.

---
+++ editors/levee/config.h
@@ -0,0 +1,29 @@
+/*
+ * configuration for levee, generated Mon Feb  8 17:30:47 EST 2016
+ * by retrobsd@debian
+ */
+#ifndef __AC_LEVEE_D
+#define __AC_LEVEE_D 1
+
+
+#define OS_LITEBSD 1
+#define SIZE 256000
+#define OS_UNIX 1
+#define HAVE_STRING_H 1
+#define HAVE_MEMSET 1
+#define HAVE_STRCHR 1
+#define HAVE_BASENAME 1
+#define HAVE_LIBGEN_H 1
+#define HAVE_SIGNAL_H 1
+#define HAVE_SIGNAL 1
+#define HAVE_TGETENT 1
+#define HAVE_TERMCAP_H 1
+#define USE_TERMCAP 1
+#define HAVE_TERMIOS_H 1
+#define HAVE_TCGETATTR 1
+#define HAVE_FIND 1
+#define HAVE_SED 1
+#define PATH_FIND "/usr/bin/find"
+#define PATH_SED "/usr/bin/sed"
+
+#endif/* __AC_LEVEE_D */
