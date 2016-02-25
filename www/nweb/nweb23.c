562164f7309843ad263a9839e11f3c6bf6420abe
Brian Callahan <dodonpachi-github@mailinator.com>
2016-02-25 15:37:15 -0500

    Remove logging to a file feature to prevent undo wear and tear on SD cards.

--- www/nweb/nweb23.c
+++ www/nweb/nweb23.c
@@ -42,30 +42,17 @@ void sigchild(int sig)
 	waitpid(-1, &status, WNOHANG | WUNTRACED);
 }
 
-void logger(int type, char *s1, char *s2, int socket_fd)
+void logger(int type, int socket_fd)
 {
-	int fd ;
-	char logbuffer[BUFSIZE*2];
 
 	switch (type) {
-	case ERROR: (void)sprintf(logbuffer,"ERROR: %s:%s Errno=%d exiting pid=%d",s1, s2, errno,getpid()); 
-		break;
 	case FORBIDDEN: 
 		(void)write(socket_fd, "HTTP/1.1 403 Forbidden\nContent-Length: 185\nConnection: close\nContent-Type: text/html\n\n<html><head>\n<title>403 Forbidden</title>\n</head><body>\n<h1>Forbidden</h1>\nThe requested URL, file type or operation is not allowed on this simple static file webserver.\n</body></html>\n",271);
-		(void)sprintf(logbuffer,"FORBIDDEN: %s:%s",s1, s2); 
 		break;
 	case NOTFOUND: 
 		(void)write(socket_fd, "HTTP/1.1 404 Not Found\nContent-Length: 136\nConnection: close\nContent-Type: text/html\n\n<html><head>\n<title>404 Not Found</title>\n</head><body>\n<h1>Not Found</h1>\nThe requested URL was not found on this server.\n</body></html>\n",224);
-		(void)sprintf(logbuffer,"NOT FOUND: %s:%s",s1, s2); 
 		break;
-	case LOG: (void)sprintf(logbuffer," INFO: %s:%s:%d",s1, s2,socket_fd); break;
 	}	
-	/* No checks here, nothing can be done with a failure anyway */
-	if((fd = open("nweb.log", O_CREAT| O_WRONLY | O_APPEND,0644)) >= 0) {
-		(void)write(fd,logbuffer,strlen(logbuffer)); 
-		(void)write(fd,"\n",1);      
-		(void)close(fd);
-	}
 	if(type == ERROR || type == NOTFOUND || type == FORBIDDEN) exit(3);
 }
 
@@ -79,7 +66,7 @@ void web(int fd, int hit)
 
 	ret =read(fd,buffer,BUFSIZE); 	/* read Web request in one go */
 	if(ret == 0 || ret == -1) {	/* read failure stop now */
-		logger(FORBIDDEN,"failed to read browser request","",fd);
+		logger(FORBIDDEN,fd);
 	}
 	if(ret > 0 && ret < BUFSIZE)	/* return code is valid chars */
 		buffer[ret]=0;		/* terminate the buffer */
@@ -87,9 +74,9 @@ void web(int fd, int hit)
 	for(i=0;i<ret;i++)	/* remove CF and LF characters */
 		if(buffer[i] == '\r' || buffer[i] == '\n')
 			buffer[i]='*';
-	logger(LOG,"request",buffer,hit);
+	logger(LOG,hit);
 	if( strncmp(buffer,"GET ",4) && strncmp(buffer,"get ",4) ) {
-		logger(FORBIDDEN,"Only simple GET operation supported",buffer,fd);
+		logger(FORBIDDEN,fd);
 	}
 	for(i=4;i<BUFSIZE;i++) { /* null terminate after the second space to ignore extra stuff */
 		if(buffer[i] == ' ') { /* string is "GET URL " +lots of other stuff */
@@ -99,7 +86,7 @@ void web(int fd, int hit)
 	}
 	for(j=0;j<i-1;j++) 	/* check for illegal parent directory use .. */
 		if(buffer[j] == '.' && buffer[j+1] == '.') {
-			logger(FORBIDDEN,"Parent directory (..) path names not supported",buffer,fd);
+			logger(FORBIDDEN,fd);
 		}
 	if( !strncmp(&buffer[0],"GET /\0",6) || !strncmp(&buffer[0],"get /\0",6) ) /* convert no filename to index file */
 		(void)strcpy(buffer,"GET /index.html");
@@ -114,16 +101,14 @@ void web(int fd, int hit)
 			break;
 		}
 	}
-	if(fstr == 0) logger(FORBIDDEN,"file extension type not supported",buffer,fd);
+	if(fstr == 0) logger(FORBIDDEN,fd);
 
 	if(( file_fd = open(&buffer[5],O_RDONLY)) == -1) {  /* open the file for reading */
-		logger(NOTFOUND, "failed to open file",&buffer[5],fd);
+		logger(NOTFOUND,fd);
 	}
-	logger(LOG,"SEND",&buffer[5],hit);
 	len = (long)lseek(file_fd, (off_t)0, SEEK_END); /* lseek to the file end to find the length */
 	      (void)lseek(file_fd, (off_t)0, SEEK_SET); /* lseek back to the file start ready for reading */
           (void)sprintf(buffer,"HTTP/1.1 200 OK\nServer: nweb/%d.0\nContent-Length: %ld\nConnection: close\nContent-Type: %s\n\n", VERSION, len, fstr); /* Header + a blank line */
-	logger(LOG,"Header",buffer,hit);
 	(void)write(fd,buffer,strlen(buffer));
 
 	/* send file in 8KB block - last block may be smaller */
@@ -177,26 +162,25 @@ int main(int argc, char **argv)
 	for(i=0;i<32;i++)
 		(void)close(i);		/* close open files */
 	(void)setpgid(0, 0);		/* break away from process group */
-	logger(LOG,"nweb starting",argv[1],getpid());
 	/* setup the network socket */
 	if((listenfd = socket(AF_INET, SOCK_STREAM,0)) <0)
-		logger(ERROR, "system call","socket",0);
+		logger(ERROR,0);
 	port = atoi(argv[1]);
 	if(port < 0 || port >65535)
-		logger(ERROR,"Invalid port number (try 1->65535)",argv[1],0);
+		logger(ERROR,0);
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	serv_addr.sin_port = htons(port);
 	if(bind(listenfd, (struct sockaddr *)&serv_addr,sizeof(serv_addr)) <0)
-		logger(ERROR,"system call","bind",0);
+		logger(ERROR,0);
 	if( listen(listenfd,64) <0)
-		logger(ERROR,"system call","listen",0);
+		logger(ERROR,0);
 	for(hit=1; ;hit++) {
 		length = sizeof(cli_addr);
 		if((socketfd = accept(listenfd, (struct sockaddr *)&cli_addr, &length)) < 0)
-			logger(ERROR,"system call","accept",0);
+			logger(ERROR,0);
 		if((pid = fork()) < 0) {
-			logger(ERROR,"system call","fork",0);
+			logger(ERROR,0);
 		}
 		else {
 			if(pid == 0) { 	/* child */
